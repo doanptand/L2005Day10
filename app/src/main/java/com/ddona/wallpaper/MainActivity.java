e19039a8faa2a32776c1dc7256592642c28a3d71
Trung Doan <doanptand@gmail.com>
2020-09-08 21:15:28 +0700

    implement list wallpaper fragment

--- app/src/main/java/com/ddona/wallpaper/MainActivity.java
+++ app/src/main/java/com/ddona/wallpaper/MainActivity.java
@@ -1,8 +1,12 @@
 package com.ddona.wallpaper;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+
+import com.ddona.wallpaper.fragment.WallpaperFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.add(R.id.fl_content, new WallpaperFragment());
+        transaction.commit();
     }
 }
